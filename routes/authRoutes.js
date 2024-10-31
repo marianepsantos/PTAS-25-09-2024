889dd77b30ad657864537147ab4cb68e2bf3437b
marianepsantos <marianepsantos15@gmail.com>
2024-10-31 16:56:37 -0400

    commit

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const AuthController = require ("../controllers/AuthController");
 
-router.post("/cadastro", (req, res) => {});
-router.post("/login", (req, res) => {});
+router.post("/cadastro", AuthController.cadastro);
+router.post("/login", AuthController.login);
 
 module.exports = router;
